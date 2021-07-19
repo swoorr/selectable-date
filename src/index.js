1f1d37b549a0b3552e904d645df8664076279705
Furkan Şahin <furkan.sahin@path.com.tr>
2021-07-20 00:11:09 +0300

    FIX: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-Vue.use({
-    param: "something"
-});
-import SelectableDate from "./SelectableDate.vue";
-
-export default {
-    install(Vue, options) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("selectable-date", SelectableDate);
-    }
-};
+import SelectableDate from './SelectableDate'
+export default SelectableDate
