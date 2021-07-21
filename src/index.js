97591bd41b0b6de759dba904b21162c8ca26fc30
Furkan Şahin <furkan.sahin@path.com.tr>
2021-07-21 16:28:22 +0300

    Update Vue

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import SelectableDatePicker from './SelectableDatePicker'
-export default SelectableDatePicker
+import SelectableDatePicker from "./SelectableDatePicker.vue";
+
+function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component("vue-selectable-datepicker", SelectableDatePicker);
+}
+
+const plugin = {
+    install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+    GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+    GlobalVue = global.vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+SelectableDatePicker.install = install;
+
+export default SelectableDatePicker;
